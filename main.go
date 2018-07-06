0ac40e68dfba02176c3342675f88667952b699ac
Andrew Klotz <agc.klotz@gmail.com>
2018-07-06 13:59:20 -0400

    basic http instrumentation

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
@@ -17,6 +19,8 @@ func main() {
 
 	// Routes
 	e.GET("/", hello)
+	e.GET("/basic", echo.WrapHandler(prometheus.InstrumentHandler("basic", instrumentedHandler())))
+	e.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
@@ -25,4 +29,11 @@ func main() {
 // Handler
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func instrumentedHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Basic instrumentation!"))
+	})
 }
